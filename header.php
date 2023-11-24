37c8875e924291d99119399a2b22217b59debf5e
DeveloperSnehal <developer.creativemarque@gmail.com>
2023-11-24 16:46:50 +0530

    created new pages

--- header.php
+++ header.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Techwix - Technology IT Solutions Consultancy HTML5 Template</title>
+    <title>BWM | Bhangarwala Waste Management</title>
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/logo/favicon.png">
 
     <!-- CSS
     ============================================ -->
